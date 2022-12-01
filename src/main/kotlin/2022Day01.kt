3c9f93cfcc13a40ecdf402e44cb03a5d97a7377b
Jon Terje Tvergrov Kalvatn <jonterje.kalvatn@gmail.com>
2022-12-01 12:17:40 +0100

    2022-01

---
+++ src/main/kotlin/2022Day01.kt
@@ -0,0 +1,13 @@
+import tools.getResourceAsText
+import tools.timer
+
+fun main() {
+  val input = getResourceAsText("01.txt").split("\n\n").map { group -> group.lines().map { it.toInt() } }
+
+  timer {
+    println(input.maxOfOrNull { it.sum() })
+  }
+  timer {
+    println(input.map { it.sum() }.sorted().takeLast(3).sum())
+  }
+}
