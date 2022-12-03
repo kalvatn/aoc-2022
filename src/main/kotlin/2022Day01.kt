9fed9f11cc3e05c43cc8701b693399e5a7c56e89
Jon Terje Tvergrov Kalvatn <jonterje.kalvatn@gmail.com>
2022-12-03 21:21:30 +0100

    .editorconfig tweaks

--- src/main/kotlin/2022Day01.kt
+++ src/main/kotlin/2022Day01.kt
@@ -1,13 +1,20 @@
 import tools.getResourceAsText
 import tools.timer
 
-fun main() {
-  val input = getResourceAsText("01.txt").split("\n\n").map { group -> group.lines().map { it.toInt() } }
+fun main() = timer {
+  val input = getResourceAsText("01.txt").split("\n\n")
+    .map { group ->
+      group.lines()
+        .map { it.toInt() }
+    }
 
   timer {
     println(input.maxOfOrNull { it.sum() })
   }
   timer {
-    println(input.map { it.sum() }.sorted().takeLast(3).sum())
+    println(input.map { it.sum() }
+      .sorted()
+      .takeLast(3)
+      .sum())
   }
 }
--- src/main/kotlin/2022Day03.kt
+++ src/main/kotlin/2022Day03.kt
@@ -1,29 +1,34 @@
 import tools.getResourceAsText
+import tools.timer
 
-fun main() {
-  val priority = (('a'..'z').zip((1..26)) + ('A'..'Z').zip((27..52))).toMap()
+fun main() = timer {
   val input = getResourceAsText("03.txt")
+  val priority = (('a'..'z').zip((1..26)) + ('A'..'Z').zip((27..52))).toMap()
 
-  val compartments = input.lines()
-    .map {
-      val n = it.length / 2
-      Pair(it.take(n), it.takeLast(n))
+  timer {
+    val compartments = input.lines()
+      .map {
+        val n = it.length / 2
+        Pair(it.take(n), it.takeLast(n))
+      }
+
+    val part1 = compartments.sumOf { (c1, c2) ->
+      c1.toSet()
+        .intersect(c2.toSet())
+        .sumOf { priority[it]!! }
     }
-
-  val part1 = compartments.sumOf { (c1, c2) ->
-    c1.toSet()
-      .intersect(c2.toSet())
-      .sumOf { priority[it]!! }
+    println(part1)
   }
-  println(part1)
 
-  val groups = input.lines()
-    .chunked(3)
+  timer {
+    val groups = input.lines()
+      .chunked(3)
 
-  val part2 = groups.sumOf { rucksacks ->
-    rucksacks.map { it.toSet() }
-      .reduce { common, k -> common.intersect(k) }
-      .sumOf { priority[it]!! }
+    val part2 = groups.sumOf { rucksacks ->
+      rucksacks.map { it.toSet() }
+        .reduce { common, k -> common.intersect(k) }
+        .sumOf { priority[it]!! }
+    }
+    println(part2)
   }
-  println(part2)
 }
